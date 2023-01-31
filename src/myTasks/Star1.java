9dccf091c080a5c0651efaac90f10678b390a643
LadaShmidt <sapteakris@gmail.com>
2023-01-31 20:01:18 +0300

    31/01/2023

--- src/myTasks/Star1.java
+++ src/myTasks/Star1.java
@@ -9,6 +9,56 @@ public class Star1 {
 	            } 
 			System.out.println();
 	      }
-	   }
-
+//////////////////////////////////////////////
+		for (int i=0; i<4; i++) {
+			for(int j=0; j<3; j++) {
+				System.out.print("*_");
+			}
+			System.out.print(" ");
+		}
+		System.out.println();
+//////////////////////////////////////////////
+		for (int i=1; i<4; i++) {
+			System.out.print(i+"_");
+			for (int j=1; j<=5; j++) {
+				System.out.print(j);
+			}
+		}
+		System.out.println();
+//////////////////////////////////////////////
+		for(int i=0; i<3; i++) {
+			System.out.println();
+			for(int j=0; j<3; j++) {
+				System.out.print("10");
+			}
+		}
+		System.out.println();
+		for(int i=0; i<3; i++) {
+			System.out.println();
+			for(int j=0; j<6; j++) {
+				if(j%2==0) {
+					System.out.print(1);
+				}else {
+					System.out.print(0);
+				}
+			}
+		}
+		System.out.println();
+//////////////////////////////////////////////
+		System.out.println();
+		for(int i=0; i<3; i++) {
+			System.out.println();
+			for(int j=1; j<7; j++) {
+				if(j%3==0) {
+					System.out.print("x");
+				}else if(j%2==0){
+					System.out.print(0);
+				}else {
+					System.out.print(1);
+				}
+			}
+		}
+		System.out.println();
+//////////////////////////////////////////////
+   }
 }
