c0bfc7403b343f893f78853dbe425376946c6df0
LadaShmidt <sapteakris@gmail.com>
2023-01-31 19:13:47 +0300

    first commit

---
+++ src/myTasks/Star1.java
@@ -0,0 +1,14 @@
+package myTasks;
+
+public class Star1 {
+	public static void main(String[]args) {
+		for(int i=1; i<=3; i++) {
+			System.out.println(i);
+				for(int l=0; l<3; l++) {
+				System.out.print("*_");
+	            } 
+			System.out.println();
+	      }
+	   }
+
+}
